5d2ea91049e51b9cfc68640b29667b6f63692519
Michael Lobman <michael.lobman@gmail.com>
2021-10-13 20:20:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+const namesArray = ["Fitz", "Mike", "Annie"];
+const messagesArray = [];
+
+function writeCards(namesArray, event) {
+    for (let i = 0; i < namesArray.length; i++) {
+        messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return messagesArray;
+}
+
+let integer = 10
+
+function countDown(integer) {
+    while (integer > -1) {
+        console.log(integer);
+        integer--;
+    }
+}
